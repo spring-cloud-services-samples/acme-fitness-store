2f6d78b4c09e0d31b129c8742ffac66c74a472b8
Kelwin Payares <kpayares@vmware.com>
2023-05-09 15:35:59 +0200

    Set Acme-Fitness accelerator as a repo branch

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 # Variable Definition 
 variable "project_name" {
   type        = string
-  default     = "fitness-store"
+  default     = "acme-fitness"
   description = "Project Name"
 }
 
@@ -9,54 +9,6 @@ variable "resource_group_location" {
   type        = string
   default     = "West Europe"
   description = "Azure Resource Group"
-}
-
-variable "asa_cart_service" {
-  type        = string
-  default     = "cart-service"
-  description = "Cart Service App Name"
-}
-
-variable "asa_order_service" {
-  type        = string
-  default     = "order-service"
-  description = "Order Service App Name"
-}
-
-variable "asa_payment_service" {
-  type        = string
-  default     = "payment-service"
-  description = "Payment Service App Name"
-}
-
-variable "asa_catalog_service" {
-  type        = string
-  default     = "catalog-service"
-  description = "Catalog Service App Name"
-}
-
-variable "asa_frontend" {
-  type        = string
-  default     = "frontend"
-  description = "Frontend App Name"
-}
-
-variable "asa_identity_service" {
-  type        = string
-  default     = "identity-service"
-  description = "Identity Service App Name"
-}
-
-variable "asa_apps" {
-  type        = list(string)
-  default     = ["catalog_service", "payment_service", "identity_service"]
-  description = "Varible used as keys to create apps"
-}
-
-variable "asa_apps_bind" {
-  type        = list(string)
-  default     = ["order_service", "cart_service", "frontend"]
-  description = "Varible used as keys to create apps with Tanzu Component Binds"
 }
 
 variable "order_service_db_name" {
@@ -72,25 +24,4 @@ variable "catalog_service_db_name" {
 variable "sso-jwk-uri" {
   type        = string
   description = "SSO Provider JWK-URI"
-}
-
-variable "sso-client-id" {
-  type        = string
-  description = "SSO Provider Client ID"
-}
-
-variable "sso-client-secret" {
-  type        = string
-  description = "SSO Provider Client Secret"
-}
-
-variable "sso-issuer-uri" {
-  type        = string
-  description = "SSO Provider Issuer URI"
-}
-
-variable "sso-scope" {
-  type        = list(string)
-  default     = ["openid", "profile", "email"]
-  description = "SSO Provider Scope"
 }
