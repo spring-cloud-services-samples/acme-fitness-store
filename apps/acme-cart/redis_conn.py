59c41be5b77723eea1c3ae0ed9ed7fd2cd8407d2
Fredrich Ombico <fredrich.ombico@broadcom.com>
2024-01-12 11:29:54 -0500

    Update redis_conn.py to read connection from VCAP_SERVICES

--- apps/acme-cart/redis_conn.py
+++ apps/acme-cart/redis_conn.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import json
 import redis
 from redislite import Redis
 from os import environ
@@ -12,11 +13,22 @@ def redis_connection(logger):
     if redis_conn_str is None and environ.get('REDIS_CONNECTIONSTRING') not in (None, ''):
         redis_conn_str = str(environ['REDIS_CONNECTIONSTRING'])
 
-    redis_host = environ['REDIS_HOST'] if environ.get('REDIS_HOST') not in (None, '') else None
-
-    redis_port = environ['REDIS_PORT'] if environ.get('REDIS_PORT') not in (None, '') else 6380
-
-    redis_password = environ['REDIS_PASSWORD'] if environ.get('REDIS_PASSWORD') not in (None, '') else None
+    # Check if VCAP_SERVICES environment variable is present for TAS environments
+    vcap_services = os.environ.get('VCAP_SERVICES')
+    if vcap_services:
+        vcap_services_json = json.loads(vcap_services)
+        if 'p-redis' in vcap_services_json:
+            redis_service = vcap_services_json['p-redis'][0]['credentials']
+            redis_host = redis_service.get('host', None)
+            redis_port = redis_service.get('port', 6380)
+            redis_password = redis_service.get('password', None)
+            print(f"Connecting to Redis - Host: {redis_host}, Port: {redis_port}")
+        else:
+            print("No Redis service found in VCAP_SERVICES. A p-redis service instance should be bound to this app")
+    else:
+        redis_host = environ['REDIS_HOST'] if environ.get('REDIS_HOST') not in (None, '') else None
+        redis_port = environ['REDIS_PORT'] if environ.get('REDIS_PORT') not in (None, '') else 6380
+        redis_password = environ['REDIS_PASSWORD'] if environ.get('REDIS_PASSWORD') not in (None, '') else None
 
     tlsEnabledEnv = auth_url = environ['REDIS_TLS_ENABLED'] if environ.get('REDIS_TLS_ENABLED') not in (None, '') else 'true'
     tlsEnabled = strtobool(tlsEnabledEnv)
